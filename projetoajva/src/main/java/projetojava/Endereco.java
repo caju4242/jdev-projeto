f533d39537d3d8f93c66759e7eba25026dfd413d
labuser <labuser@DESKTOP-SBMTP89.mshome.net>
2021-08-08 10:38:40 -0300

    Adicionando a classe Endereco

---
+++ projetoajva/src/main/java/projetojava/Endereco.java
@@ -0,0 +1,28 @@
+package projetojava;
+
+public class Endereco {
+	String nomeDaRua;
+	String bairro;
+	String cidade;
+	
+	public String getNomeDaRua() {
+		return nomeDaRua;
+	}
+	public void setNomeDaRua(String nomeDaRua) {
+		this.nomeDaRua = nomeDaRua;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	
+}
