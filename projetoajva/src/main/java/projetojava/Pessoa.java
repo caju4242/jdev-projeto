40a1b60a47cb52f62cfb808102fbef60e5469abc
labuser <labuser@DESKTOP-SBMTP89.mshome.net>
2021-08-08 10:55:00 -0300

    Adicionando Getters and Setters

--- projetoajva/src/main/java/projetojava/Pessoa.java
+++ projetoajva/src/main/java/projetojava/Pessoa.java
@@ -5,4 +5,25 @@ public class Pessoa {
 	int idade;
 	Endereco endereco;
 	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	
 }
